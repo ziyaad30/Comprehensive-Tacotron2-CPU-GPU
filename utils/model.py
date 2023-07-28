bc634c2cbb51d50826823c8f4be0bcfe6700e61d
Ziyaad <xavierbaatjes@yahoo.com>
2023-07-28 14:06:56 +0200

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -17,7 +17,10 @@ def get_model(args, configs, device, train=False):
             train_config["path"]["ckpt_path"],
             "{}.pth.tar".format(args.restore_step),
         )
-        ckpt = torch.load(ckpt_path)
+        if torch.cuda.is_available():
+            ckpt = torch.load(ckpt_path)
+        else:
+            ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
         model.load_state_dict(ckpt["model"])
 
     if train:
